d6769e8718ab7813a9633975b0e1b08119fef8f3
AlexZhuHai <5030749@qq.com>
2017-04-27 19:11:54 +0800

    Update App.java

--- mvnDemo/src/main/java/com/alex/mvnDemo/App.java
+++ mvnDemo/src/main/java/com/alex/mvnDemo/App.java
@@ -12,6 +12,7 @@ public class App
         System.out.println( "Hello World 2 " );
         System.out.println( "Hello World 3 " );
         System.out.println( "Hello World 4 " );
-        System.out.println( "Hello docker 3 " );
+        System.out.println( "Hello docker 1 " );
+        System.out.println( "Hello docker 2 " );
     }
 }
