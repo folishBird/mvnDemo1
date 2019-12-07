ab3f112b80a8022e150b0a31559735968dcfaead
foolish <32386714+folishBird@users.noreply.github.com>
2019-12-07 11:57:19 +0800

    Update App.java

--- mvnDemo/src/main/java/com/alex/mvnDemo/App.java
+++ mvnDemo/src/main/java/com/alex/mvnDemo/App.java
@@ -14,7 +14,9 @@ public class App
         System.out.println( "Hello World 4 " );
         System.out.println( "Hello docker 1 " );
         System.out.println( "Hello docker 2 " );
+
         System.out.println( "Hello docker 3 " );
+        System.out.println( "Hello docker 5 " );
         
     }
 }
